a4abb5c6aee13af0560b935cfd962f6d13d78f9b
okazaki2007 <86208462+okazaki2007@users.noreply.github.com>
2021-10-15 23:56:31 -0600

    Method "add first" added

--- N-ary_tree/src/TreeNode.java
+++ N-ary_tree/src/TreeNode.java
@@ -1,34 +1,83 @@
-import java.util.Objects;
+import org.w3c.dom.Node;
+
 import java.util.Vector;
 
 class TreeNode {
-    public int key;
+    public String valor;
+    private Node siguiente;
     public Vector<TreeNode> child;
 
-    public TreeNode(int key) {
+    public TreeNode(String valor) {
         // Establecer llave del nodo
-        this.key = key;
+        this.valor = valor;
         // crear memoria del hijo de TreeNode
         this.child = new Vector<TreeNode>();
     }
 
-    public void addChild(int key) {
-        // Create a new node
-        TreeNode node = new TreeNode(key);
-        // Add node into child
-        this.child.add(node);
+    public String getValor() {
+        return valor;
     }
 
 
-    public static class NAryTree {
-        public TreeNode root;
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Node getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Node siguiente) {
+        this.siguiente = siguiente;
+    }
 
 
-        public NAryTree() {
+    public void addChild(String valor) {
+        // Create a new node
+        TreeNode node = new TreeNode(valor);
+        // Add node into child
+        this.child.add(node);
 
-            this.root = null;
+
+    }
+    public void addFirst( String valor){
+        TreeNode node = new TreeNode(valor);
+        if(NAryTree.inicio.child==null){
+            NAryTree.inicio=node;
+
+        }else{
+           this.valor=valor+" "+getValor();
+
+
+
+
 
         }
+    }
+
+
+
+
+
+
+
+
+
+    // Add node into child
+
+
+
+
+
+
+
+    public static class NAryTree {
+        static TreeNode inicio;
+        int tamaño;
+
+
+
+
 
 
         public void deleteLeafNodes(TreeNode node) {
@@ -41,6 +90,7 @@ class TreeNode {
             TreeNode temp = null;
 
             while (i < node.child.size()) {
+                tamaño++;
                 temp = node.child.get(i);
 
                 if (temp.child.size() == 0) {
@@ -53,24 +103,26 @@ class TreeNode {
                 }
             }
 
+
         }
 
         public void removeLeafNodes() {
 
-            if (this.root == null) {
+            if (this.inicio == null) {
                 // cuando el arbol es vacio
                 return;
             }
-            if (this.root.child.size() == 0) {
+            if (this.inicio.child.size() == 0) {
                 // cuando elimina la raiz del nodo
-                this.root = null;
+                this.inicio = null;
             } else {
                 // encontrar y elimnar nodo hojas
-                this.deleteLeafNodes(this.root);
+                this.deleteLeafNodes(this.inicio);
             }
 
 
         }
+
 
 
         public void printPreorder(TreeNode node) {
@@ -81,10 +133,12 @@ class TreeNode {
             int i = 0;
 
             TreeNode temp = null;
-            System.out.print("  " + node.key);
+            System.out.print("  " + node.valor);
 
             // Iterando el hijo del nodo dado
             while (i < node.child.size()) {
+
+
                 temp = node.child.get(i);
 
                 printPreorder(temp);
@@ -94,39 +148,54 @@ class TreeNode {
 
 
         }
-
-
     }
+
+
+
     public static void main(String[] args) {
         NAryTree tree = new NAryTree();
 
         // Primer elemento del arbol
-        tree.root = new TreeNode(10);
-        tree.root.addChild(8);
-        tree.root.addChild(5);
+
+        tree.inicio = new TreeNode("10");
+
+        tree.inicio.addChild("8");
+
+        tree.inicio.addChild("5");
+        tree.inicio.addFirst( "12000");
+        tree.inicio.addFirst( "444444444");
+
+
+
         // agregar nodos hijo [-2,1,6] en nodo (8)
-        tree.root.child.get(0).addChild(-2);
-        tree.root.child.get(0).addChild(1);
-        tree.root.child.get(0).addChild(6);
+
+        tree.inicio.child.get(0).addChild("-2");
+        tree.inicio.child.get(0).addChild("1");
+        tree.inicio.child.get(0).addChild("6");
         // agregar nodos hijo [9,11] en nodo (1)
-        tree.root.child.get(0).child.get(1).addChild(9);
-        tree.root.child.get(0).child.get(1).addChild(11);
+        tree.inicio.child.get(0).child.get(1).addChild("9");
+        tree.inicio.child.get(0).child.get(1).addChild("11");
+
+
         // agregar nodos hijo  [17  12] en nodo (11)
-        tree.root.child.get(0).child.get(1).child.get(1).addChild(17);
-        tree.root.child.get(0).child.get(1).child.get(1).addChild(12);
+        tree.inicio.child.get(0).child.get(1).child.get(1).addChild("17");
+        tree.inicio.child.get(0).child.get(1).child.get(1).addChild("12");
         // agregar nodos hijo  [7 18 3  4] en nodo(5)
-        tree.root.child.get(1).addChild(7);
-        tree.root.child.get(1).addChild(18);
-        tree.root.child.get(1).addChild(3);
-        tree.root.child.get(1).addChild(4);
+        tree.inicio.child.get(1).addChild("7");
+        tree.inicio.child.get(1).addChild("18");
+        tree.inicio.child.get(1).addChild("3");
+        tree.inicio.child.get(1).addChild("4");
 
         // agregar nodo hijo [-1] en nodo(7)
-        tree.root.child.get(1).child.get(0).addChild(-1);
+        tree.inicio.child.get(1).child.get(0).addChild("-1");
 
         // agregar nodos hijo [2,1,3] en nodo(4)
-        tree.root.child.get(1).child.get(3).addChild(2);
-        tree.root.child.get(1).child.get(3).addChild(1);
-        tree.root.child.get(1).child.get(3).addChild(3);
+        tree.inicio.child.get(1).child.get(3).addChild("2");
+        tree.inicio.child.get(1).child.get(3).addChild("1");
+        tree.inicio.child.get(1).child.get(3).addChild("3");
+
+
+
 
         System.out.print("\n Antes de remover nodos hojas \n");
 
@@ -149,7 +218,8 @@ class TreeNode {
             -----------------------
             Constructing N-Ary tree
         */
-        tree.printPreorder(tree.root);//imprime componentes de hojas
+        tree.printPreorder(tree.inicio);//imprime componentes de hojas
+
 
 
         // Remover nodos hijos
@@ -170,7 +240,7 @@ class TreeNode {
         */
 
 
-        tree.printPreorder(tree.root);
+        tree.printPreorder(tree.inicio);
 
     }
 }
